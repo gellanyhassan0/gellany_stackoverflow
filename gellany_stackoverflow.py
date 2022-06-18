2b5c839dd5202bb5344338213428bdf1bb9335aa
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-18 10:13:15 +0200

    Add files via upload

---
+++ gellany_stackoverflow.py
@@ -0,0 +1,32 @@
+class gellany_stackoverflow():
+    
+# init method or constructor
+    def __init__(self, file = None):
+                  
+                 self.file = file
+          
+
+    def sha256(self):
+
+       import hashlib
+         #with open("text.txt", "rb") as f: #for int_bytes in f:
+       try:
+           for f in range(0,10):
+
+              int_bytes = bytes((str(f)+'\n'), 'utf-8')
+              print(int_bytes)
+              readable_hash = hashlib.sha256(int_bytes).hexdigest()
+              print(readable_hash)
+
+       except: 
+            with open(self.file, "rb") as f: 
+              for int_bytes in f:
+                   int_bytes = bytes((str(f)+'\n'), 'utf-8')
+                   print(int_bytes)
+                   readable_hash = hashlib.sha256(int_bytes).hexdigest()
+                   print(readable_hash)
+
+
+
+gellany_stackoverflow().sha256()
+gellany_stackoverflow(file = 'text.txt').sha256()
